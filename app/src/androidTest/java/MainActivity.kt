60a42e58e84febacb4492c3869be76c2fd0a2a5c
Nayan Ghodadara <nayanghodadara187@gmail.com>
2024-12-08 13:37:06 +0530

    style(app): minor ui changes

---
+++ app/src/androidTest/java/MainActivity.kt
@@ -0,0 +1,18 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gradientview.CustomTextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Dash animation
+        findViewById<CustomTextView>(R.id.customTextView)
+            .Animation()
+            .setDashAnimation()
+            .speed(1000)
+            .setDirection(CustomTextView.Direction.FORWARD)
+    }
+}
